9ad6bbab95d6d40b398a134345b3b02b185e2bcc
Arya Putrarizky <aryaputrarizky4@gmail.com>
2026-01-30 21:17:56 +0700

    fix duplicate db require

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const db = require('./db');
 
 const app = express();
 app.use(express.json());
-
 const ADMIN_USERNAME = 'botpromomurah';
-const TelegramBot = require('node-telegram-bot-api');
+
 
 const bot = new TelegramBot(process.env.BOT_TOKEN);
 
